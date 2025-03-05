cc1359f4c8f0b84ae81c2dd0ab84c200332a0695
Yuqi Zhou <86260893+yuqizhou77@users.noreply.github.com>
2025-03-05 16:57:41 +0800

    build: update prerelease-feaure-control script to make it more general (#13345)

--- .github/scripts/prerelease-feature-control.sh
+++ .github/scripts/prerelease-feature-control.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 filePath=packages/vscode-extension/src/releaseBasedFeatureSettings.ts
 echo "Update feature settings in $filePath if alpha or preview release"
-#sed -i -e "s@const shouldEnableTeamsCopilotChatUI = false@const shouldEnableTeamsCopilotChatUI = true@g" $filePath
+varNames=$(grep -o "const [a-zA-Z0-9_]\+" $filePath | grep -v "releaseControlledFeatureSettings" | sed 's/const //')
+
+# Process each variable
+for varName in $varNames; do
+  # Check if current value is false
+  if grep -q "$varName = false" $filePath; then
+    echo "Setting $varName to true"
+    sed -i -e "s@const $varName = false@const $varName = true@g" $filePath
+  else
+    echo "$varName is already set to true or has another value"
+  fi
+done
+
 echo "Prerelease feature setting update done."
