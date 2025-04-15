85ded37d584e7626c24bdd2cf5354e04f178d3e9
Huajie Zhang <huajiezhang@microsoft.com>
2025-04-15 11:06:08 +0800

    fix: no valid app catelog error message (#13695)
    * fix: no valid app catelog error message
    
    * fix: no valid app catelog error message
    
    * fix: no valid app catelog error message
    
    * fix: no valid app catelog error message

--- packages/fx-core/src/component/driver/deploy/spfx/error/noValidAppCatelogError.ts
+++ packages/fx-core/src/component/driver/deploy/spfx/error/noValidAppCatelogError.ts
@@ -3,7 +3,7 @@
 
 import { UserError } from "@microsoft/teamsfx-api";
 import { getDefaultString, getLocalizedString } from "../../../../../common/localizeUtils";
-import { MetadataV4 } from "../../../../../common/versionMetadata";
+import { MetadataV3, MetadataV4 } from "../../../../../common/versionMetadata";
 import { Constants } from "../utility/constants";
 
 const errorCode = "NoValidAppCatelog";
@@ -11,11 +11,12 @@ const messageKey = "driver.spfx.error.noValidAppCatelog";
 
 export class NoValidAppCatelog extends UserError {
   constructor() {
+    const ymlFile = `"${MetadataV4.configFile}" or "${MetadataV3.configFile}"`;
     super({
       source: Constants.DeployDriverName,
       name: errorCode,
-      message: getDefaultString(messageKey, MetadataV4.configFile),
-      displayMessage: getLocalizedString(messageKey, MetadataV4.configFile),
+      message: getDefaultString(messageKey, ymlFile),
+      displayMessage: getLocalizedString(messageKey, ymlFile),
     });
   }
 }
